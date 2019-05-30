c8ea45c6900f16f840bcbeb9bf44432ef64c5b2a
benoitVaisse <benoit.vaisse@gmail.com>
2019-05-30 12:04:57 +0100

    creation methode pour afficher une seule annonce et creation de son fichier twig, chabement block title de base.html.twig

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,6 +14,8 @@ class AdController extends AbstractController
     /**
      * affiche toutes les annonces du site
      * @Route("/ads", name="ads_liste")
+     * 
+     * @return Response
      */
     public function index(AdRepository $adRepo, ObjectManager $manager)
     {
@@ -20,6 +23,21 @@ class AdController extends AbstractController
         $ads = $adRepo->findAll();
         return $this->render('ad/ads_liste.html.twig', [
             "ads"=>$ads 
+            ]);
+    }
+
+
+    /**
+     * fonction qui permet d'afficher l'annonce sélectionné
+     * @Route("/ad/{slug}", name="ad_one")
+     *
+     * @param Ad $ad
+     * @return Response
+     */
+    public function showOne(Ad $ad)
+    {
+        return $this->render('ad/ad_one.html.twig', [
+            "ad"=>$ad
             ]);
     }
 }
