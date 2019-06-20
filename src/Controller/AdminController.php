272d72a4fc42943a33a99b1fed0dbed48c77b515
benoitVaisse <benoit.vaisse@gmail.com>
2019-06-20 16:38:30 +0100

    mise en place de la scurité pour ce connecter au compte admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminController extends AbstractController
 {
@@ -17,16 +19,23 @@ class AdminController extends AbstractController
      * @Route("/admin/login", name="admin_login")
      * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        if($error != null)
+        {
+            $this->addFlash("danger","L'email ou le password est invalide");
+        }
 
-        return $this->render("admin/login.html.twig", []);
+        return $this->render("admin/account/login.html.twig");
     }
 
 
     /**
      * permet a l'admin de ce déconnecter
      *
+     * @Route("/admin/logout", name="admin_logout")
      * @return void
      */
     public function logout(){
@@ -37,6 +46,7 @@ class AdminController extends AbstractController
 
     /**
      * @Route("/admin/ads", name="admin_ads")
+     * @Security("is_granted('ROLE_ADMIN')")
      */
     public function index(AdRepository $adRepo)
     {
